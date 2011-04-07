673a237c942fde77a1b805cae481299d61e72a53
Bhargav Mangipudi <bhargav.1191@gmail.com>
2011-04-07 23:55:46 +0530

    Refactor data structures

--- zrp_rtable.h
+++ zrp_rtable.h
@@ -15,34 +15,6 @@
 //		IERP Routing Table
 
 
-//		Bordercast Tree table Entry
 
-class zrp_btt_entry {
-	friend class zrp_bttable;
-	friend class ZRP;
-
-public:
-	zrp_btt_entry();
-	~zrp_btt_entry();
-
-private:
-
-	ns_addr_t node_id;
-	bool member;
-
-	LIST_ENTRY(nodelist_entry) dwnstrm_n;				 // Downstream Neighbor List
-	LIST_ENTRY(nodelist_entry) dwnstrm_pnodes;			//  Downstream Peripheral Nodes List
-};
-
-//		Bordercast Tree Table
-
-class zrp_bttable {
-public:
-	zrp_bttable() { LIST_INIT(&btthead); }
-
-	zrp_btt_entry*	head() { return btthead.lh_first; }
-private:
-	LIST_HEAD(zrp_btthead, zrp_btt_entry) btthead;
-};
 
 #endif
